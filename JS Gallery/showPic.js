4a67ba04d1beb4a631ff37f5502007ec7a7ba777
byAlice <1533674340@qq.com>
2016-05-25 23:39:15 +0800

    image gallery  modified again

--- JS Gallery/showPic.js
+++ JS Gallery/showPic.js
@@ -10,6 +10,7 @@
  var description = document.getElementById("description");
  description.firstChild.nodeValue = text;
  }*/
+/*
 window.onload = function () {
 
     prepareGallery();
@@ -28,9 +29,9 @@ function prepareGallery() {
         }
     }
 }
-/*如果图片切换成功，showPic()函数应该返回false；
+/!*如果图片切换成功，showPic()函数应该返回false；
   如果图片切换不成功，showPic()函数应该返回true；
-*/
+*!/
 function showPic(whichpic) {
     if (!document.getElementById("placeholder")) return true; //如果图片不存在，继续执行
     var source = whichpic.getAttribute("href");
@@ -45,3 +46,82 @@ function showPic(whichpic) {
     }
     return false; //切换图片和刷新description元素都取得成功，此时必须取消onclick事件默认行为
 }
+*/
+/*window.onload = function () {
+    preparePlaceholder();
+    prepareGallery();
+
+
+};*/
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != "function"){
+        window.onload = func;
+    }
+    else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+function insertAfter(newElement,targetElement){
+    var parent = targetElement.parentNode;
+    if(parent.lastChild == targetElement){
+        parent.appendChild(newElement);
+    }
+    else{
+        parent.insertBefore(newElement,targetElement.nextSibling)
+    }
+}
+function preparePlaceholder(){
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","img/1.jpg");
+    placeholder.setAttribute("alt","my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,gallery);
+}
+function prepareGallery(){
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(var i = 0;i< links.length;i++) {
+        links[i].onlick = function(){
+            return showPic(this);
+        };
+        links[i].onkeypress = links[i].onclick;
+    }
+}
+function showPic(whichpic){
+    if(!document.getElementById("placeholder")) return true;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+    if(!document.getElementById("description")) return false;
+    if(whichpic.getAttribute("title")){
+        var text = whichpic.getAttribute("title");
+    }
+    else{
+        text = "";
+    }
+    var description = document.getElementById("description");
+    if(description.firstChild.nodeType ==3){
+        description.firstChild.nodeValue = text;
+    }
+    return false;
+}
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
