168464b1ec6d399204bc78d4799a83f3874057ee
byalice <1533674340@qq.com>
2016-06-10 18:37:56 +0800

    try atom git-plus

--- delay_tooltip/1.js
+++ delay_tooltip/1.js
@@ -1,14 +1,14 @@
-window.onload=function () {
-    var oDiv1=document.getElementById('div1');
-    var oDiv2=document.getElementById('div2');
-    var timer=null;
-    oDiv2.onmouseover=oDiv1.onmouseover=function () {
+window.onload = function() {
+    var oDiv1 = document.getElementById('div1');
+    var oDiv2 = document.getElementById('div2');
+    var timer = null;
+    oDiv2.onmouseover = oDiv1.onmouseover = function() {
         clearTimeout(timer);
-        oDiv2.style.display= 'block';
-    }
-    oDiv2.onmouseout=oDiv1.onmouseout=function () {
-        timer=setTimeout(function () {
-            oDiv2.style.display= 'none';
-        },500);
+        oDiv2.style.display = 'block';
+    };
+    oDiv2.onmouseout = oDiv1.onmouseout = function() {
+        timer = setTimeout(function() {
+            oDiv2.style.display = 'none';
+        }, 500);
     };
 };
