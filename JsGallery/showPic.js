715fe3fc423f3637cda5c5490a82050e447b73d1
byalice <1533674340@qq.com>
2016-06-12 22:00:45 +0800

    img

--- JsGallery/showPic.js
+++ JsGallery/showPic.js
@@ -1,5 +1,5 @@
 /**
- * Created by lenovo on 2016/5/24.
+ * Created byalice on 2016/5/24.
  */
 /*
  function showPic(whichpic){
@@ -18,33 +18,31 @@ window.onload = function () {
 };
 
 function prepareGallery() {
-    if (!document.getElementsByTagName) return false;//检查当前浏览器是否理解getElementsByTagName（）的方法；不理解就离开，理解就继续执行
-    if (!document.getElementById) return false;//检查当前浏览器是否理解getElementById()的方法，不理解就离开，理解就继续执行
-    if (!document.getElementById("imagegallery")) return false;//检查当前网页是否包含一个id属性值是"imagegallery"的元素，不理解就离开，理解就继续执行
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
     var gallery = document.getElementById("imagegallery");
     var links = gallery.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {    //遍历数组中的链接（links相当于节点列表）
-        links[i].onclick = function () {        //给数组中链接添加onclick事件（onclick也很好的支持键盘事件）
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
             return showPic(this);
         }
     }
 }
-/!*如果图片切换成功，showPic()函数应该返回false；
-  如果图片切换不成功，showPic()函数应该返回true；
-*!/
+
 function showPic(whichpic) {
-    if (!document.getElementById("placeholder")) return true; //如果图片不存在，继续执行
+    if (!document.getElementById("placeholder")) return true;
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
-    if (placeholder.nodeName != "IMG") return true;  //
-    placeholder.setAttribute("src", source); //切换图片
-    if (!document.getElementById("description")) return false; //如果图片切换成功，即使description元素未被刷新，也要取消onclick事件默认行为
-    var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";    //如果有title值，就把值赋给text，如果title值为空，text值为null
+    if (placeholder.nodeName != "IMG") return true;
+    placeholder.setAttribute("src", source);
+    if (!document.getElementById("description")) return false;
+    var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
     var description = document.getElementById("description");
-    if (description.firstChild.nodeType == 3) {     //判断节点属性是不是文本属性
+    if (description.firstChild.nodeType == 3) {
         description.firstChild.nodeValue = text;
     }
-    return false; //切换图片和刷新description元素都取得成功，此时必须取消onclick事件默认行为
+    return false;
 }
 */
 /*window.onload = function () {
@@ -55,7 +53,7 @@ function showPic(whichpic) {
 };*/
 
 
-/*页面加载项*/
+
 function addLoadEvent(func){
     var oldonload = window.onload;
     if(typeof window.onload != "function"){
@@ -68,7 +66,7 @@ function addLoadEvent(func){
         }
     }
 }
-/*insertAfter（）函数*/
+
 function insertAfter(newElement,targetElement){
     var parent = targetElement.parentNode;
     if(parent.lastChild == targetElement){
@@ -78,15 +76,15 @@ function insertAfter(newElement,targetElement){
         parent.insertBefore(newElement,targetElement.nextSibling)
     }
 }
-/*preparePlaceholder（）函数*/
+
 function preparePlaceholder(){
     if(!document.createElement) return false;
     if(!document.createTextNode) return false;
     if(!document.getElementById) return false;
     if(!document.getElementById("imagegallery")) return false;
-    var placeholder = document.createElement("images");
+    var placeholder = document.createElement("img");
     placeholder.setAttribute("id","placeholder");
-    placeholder.setAttribute("src", "images/1.jpg");
+    placeholder.setAttribute("src", "img/1.jpg");
     placeholder.setAttribute("alt","my image gallery");
     var description = document.createElement("p");
     description.setAttribute("id","description");
@@ -98,7 +96,7 @@ function preparePlaceholder(){
     insertAfter(placeholder,gallery);
 }
 
-/*prepareGallery（）函数*/
+
 function prepareGallery(){
     if(!document.getElementsByTagName) return false;
     if(!document.getElementById) return false;
@@ -109,11 +107,10 @@ function prepareGallery(){
         links[i].onclick = function(){
             return showPic(this);
         };
-        links[i].onkeypress = links[i].onclick;     //键盘点击动作
+        links[i].onkeypress = links[i].onclick;
     }
 }
 
-/*showPic（）函数*/
 function showPic(whichpic){
     if(!document.getElementById("placeholder")) return true;
     var source = whichpic.getAttribute("href");
@@ -132,6 +129,6 @@ function showPic(whichpic){
     }
     return false;
 }
-/*要加载的函数*/
+/*瑕佸姞杞界殑鍑芥暟*/
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
