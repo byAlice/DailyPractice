6ce4745228f633bf157f488e474cf89db6e9c8cc
byalice <1533674340@qq.com>
2016-06-21 17:49:27 +0800

    sort rank

--- table/js/2.js
+++ table/js/2.js
@@ -2,26 +2,26 @@
  * Created by alice on 2016/6/19.
  */
 window.onload = function () {
-    var oUl1 = document.getElementById('ul1');
+    var oTab = document.getElementById('tab1');
     var oBtn = document.getElementById('btn1');
 
     oBtn.onclick = function () {
-        var aLi = oUl1.getElementsByTagName('li');
+        //var aTr = oTab1.getElementsByTagName('tr');
         var arr = [];
 
-        for (var i = 0; i < aLi.length; i++) {
-            arr[i] = aLi[i];
+        for (var i = 0; i < oTab.tBodies[0].rows.length; i++) {
+            arr[i] = oTab.tBodies[0].rows[i];
         }
 
-        arr.sort(function (li1, li2) {
-            var n1 = parseInt(li1.innerHTML);   //将li1标签中的数字以10为基数返回成整数，存入n1中
-                var n2 = parseInt(li2.innerHTML);
+        arr.sort(function (tr1, tr2) {
+                var n1 = parseInt(tr1.cells[0].innerHTML);   //将tr中第一个单元格的数字以10为基数返回成整数
+                var n2 = parseInt(tr2.cells[0].innerHTML);
                 return n1 - n2;
             }
         );
 
         for (i = 0; i < arr.length; i++) {
-            oUl1.appendChild(arr[i]);
+            oTab.tBodies[0].appendChild(arr[i]);
         }
     }
 };
