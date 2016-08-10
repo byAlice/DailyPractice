8bb33e590050744a3c4cd1a04df39f41227f0ea0
byalice <1533674340@qq.com>
2016-08-11 01:52:50 +0800

    duitang nav2

--- ImitateDuiTang/js/master.js
+++ ImitateDuiTang/js/master.js
@@ -2,19 +2,26 @@
  * Created by alice on 2016/8/9.
  */
 //下拉框
-(function () {
-   $('[data-toggle=arrowdown]').hover(function () {
+$(document).ready(function () {
+   $('.classify').hover(function () {
       $('.classify').addClass('classify-hover');
-       $('.icon-bottom').removeClass('run-down');
-       $('.icon-bottom').addClass('run-up');
-      $('#move').css('display','block');
+       $('.icon-bottom').removeClass('run-down').addClass('run-up');
+      $('#more').css('display','block');
    },function () {
        $('.classify').removeClass('classify-hover');
-       $('.icon-bottom').removeClass('run-up');
-       $('.icon-bottom').addClass('run-down');
-       $('#move').css('display','none');
+       $('.icon-bottom').removeClass('run-up').addClass('run-down');
+       $('#more').css('display','none');
    });
-    $('[data-toggle=hidden-box]').hover(function () {
-        
-    })
-})(jQuery);
+    $('#more').hover(function () {
+        $('.classify').addClass('classify-hover');
+        $('.icon-bottom').removeClass('run-down').addClass('run-up');
+        $('#more').css('display','block');
+    },function () {
+        $('.classify').removeClass('classify-hover');
+        $('.icon-bottom').removeClass('run-up').addClass('run-down');
+        setTimeout(function () {
+            $(".icon-bottom").removeClass('run-down');
+        },500);
+        $('#more').css('display','none');
+    });
+});
